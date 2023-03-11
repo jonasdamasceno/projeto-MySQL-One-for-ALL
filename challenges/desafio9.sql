3d98d9c09304f610bb244a4f10a3b5b9cfd5ae4e
jonasdamasceno <jonasmichael2021@outlook.com>
2023-03-11 08:06:56 -0300

    fix: consertanto erro de digitação no requisto 9

--- challenges/desafio9.sql
+++ challenges/desafio9.sql
@@ -1,6 +1,6 @@
 SELECT
     COUNT(payback_history.music_id) AS musicas_no_historico
 FROM payback_history
-    INNER JOIN users ON user.user_id = payback_history.user_id
+    INNER JOIN users ON users.user_id = payback_history.user_id
 WHERE
-    user.user_name = 'Barbara Liskov';
+    users.user_name = 'Barbara Liskov';
